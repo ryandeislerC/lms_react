1ce61ce05f01c1c34e08f09f7fec1b624fd5cc57
Ryan <ryan.deisler@capgemini.com>
2021-10-05 14:07:11 -0400

    Changed header to use react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import Home from './components/home/home';
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,25 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './header.css';
 <header className="css"></header>
 function Header() {
     return(
         <div>
-            <div className='header'>
-                <div>
-                    <h2>The SKKRT Library System</h2>
-                </div>
-                <div>
-                    <Link to='/'>Home</Link>
-                </div>
-                <div id='profileSection'>
-                    <Link to='/books'>Books</Link>
-                </div>
-
-
-                <div>
-                    <a href='/logout'>Logout</a>
-                </div>
-            </div>
+            <Navbar bg='primary'>
+                <Container>
+                    <Navbar.Brand href="/">The SKRRT Library</Navbar.Brand>
+                    <Nav>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <Nav.Link href='/books'>Books</Nav.Link>
+                    </Nav>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                    Signed in as: <a href="/logout">Mark Otto</a>
+                    </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }
